1562b35e41aa34c280e6dc48dab15075eff6801e
Alex De Biasi <debia.hs@gmail.com>
2018-06-04 22:19:09 +0100

    Project Setup

---
+++ src/main/java/it/adebiasi/primes/primeservice/PrimeServiceApplication.java
@@ -0,0 +1,12 @@
+package it.adebiasi.primes.primeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeServiceApplication.class, args);
+    }
+}
